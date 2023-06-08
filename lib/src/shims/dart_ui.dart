9e2573ecba6aed008da46d55a9f64940927c2043
Shreejeet Praveen <praveen.shreejeet@gmail.com>
2023-06-08 11:39:01 +0530

    init

---
+++ lib/src/shims/dart_ui.dart
@@ -0,0 +1,8 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// This file shims dart:ui in web-only scenarios, getting rid of the need to
+/// suppress analyzer warnings.
+// a dedicated place, https://github.com/flutter/flutter/issues/55000
+export 'dart_ui_fake.dart' if (dart.library.html) 'dart_ui_real.dart';
